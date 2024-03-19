18a3a0516b9dc205757a056069a861fe615eb918
Prateek <prateekpradhan80@gmail.com>
2024-03-19 23:18:21 +0530

    first

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -7,18 +7,22 @@ const Navbar = () => {
     <>
       <div>
         <header className="bg-white border-b border-x-gray-200 ">
-          <nav className="conatainer   flex justify-between p-1 items-center">
-            <Link to='/'>
-              <img className="w-48 h-25 ml-4 " src={localstore} alt="logo" />
+          <nav className="conatainer   flex justify-between p-1  ">
+            <Link to="/">
+              <img className="w-48 h-25  " src={localstore} alt="logo" />
             </Link>
-            <div className="flex  items-center justify-center text-lg  ">
-              <Link className="mr-5 " to="/" >Home</Link>
-              <Link className="mr-5">All Products</Link>
-              <Link className="mr-5">Men</Link>
-              <Link className="mr-5">Kids</Link>
+            <div className="flex  items-center justify-center  font-semibold text-xl  ">
+              <Link className="mr-7 " to="/">
+                Home
+              </Link>
+              <Link className="mr-7">All Products</Link>
+              <Link className="mr-7">Men</Link>
+              <Link className="mr-7">Kids</Link>
             </div>
             <div className="flex justify-center items-center gap-10 mr-20 ">
-              <Link className="inline-flex items-center " to='/cart'>Cart</Link>
+              <Link className="inline-flex items-center " to="/cart">
+                Cart
+              </Link>
               <Link>Log in</Link>
             </div>
           </nav>
